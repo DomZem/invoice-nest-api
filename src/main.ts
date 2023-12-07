6704f49aae1461ed4271e7b6275483b941d24f14
Dominik <dominik.zemlik.dev@gmail.com>
2023-12-07 10:40:05 +0100

    Create database exception filter to catch database error in whole app

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DatabaseExceptionFilter } from './common/filter/database-exception.filter';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
+  app.useGlobalFilters(new DatabaseExceptionFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
 
   await app.listen(3000);
 }
